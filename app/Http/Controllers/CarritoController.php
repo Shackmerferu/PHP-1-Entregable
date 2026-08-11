51c04fbc6bde8f67378f3b0bbe9539ef62147170
Shackmerferu <lucianocayssials@gmail.com>
2026-08-10 23:56:03 -0300

    refactor: quitar extends Controller inexistente, importar CarritoController en rutas y eliminar TiendaController vacío

--- app/Http/Controllers/CarritoController.php
+++ app/Http/Controllers/CarritoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CarritoController extends Controller
+class CarritoController
 {
     /**
      * Display a listing of the resource.
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarritoController;
 use App\Http\Controllers\ProductoController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
