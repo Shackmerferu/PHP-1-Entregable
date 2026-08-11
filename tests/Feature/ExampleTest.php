0b0004860d25c532d59307d07ae01e71bf8c68e7
Shackmerferu <lucianocayssials@gmail.com>
2026-08-11 00:24:14 -0300

    test: adaptar ExampleTest al redireccionamiento de la portada y al listado de productos

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,13 @@
 <?php
 
-test('the application returns a successful response', function () {
-    $response = $this->get('/');
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
-    $response->assertStatus(200);
+uses(RefreshDatabase::class);
+
+test('la portada redirige al listado de productos', function () {
+    $this->get('/')->assertRedirect('/productos');
+});
+
+test('el listado de productos responde correctamente', function () {
+    $this->get('/productos')->assertSuccessful();
 });
